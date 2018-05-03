cfb4ffee860106fd0c6565f344ab91aeb20a419e
成艺 <353805991@qq.com>
2018-05-03 15:30:37 +0800

    2018-05-03

--- dialogloading/src/main/java/com/scy/dialogloading/LoadingView.java
+++ dialogloading/src/main/java/com/scy/dialogloading/LoadingView.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.animation.LinearInterpolator;
